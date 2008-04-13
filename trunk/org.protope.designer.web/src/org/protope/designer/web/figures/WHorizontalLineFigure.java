7db050b854b8e759e99a26f86b9c247dd4eff4d7
hakandilek <hakandilek@2b12a20e-354a-0410-ac62-c160d17b8e0a>
2008-04-13 22:41:33 +0000

    Issue 10:Add web palette components

---
+++ trunk/org.protope.designer.web/src/org/protope/designer/web/figures/WHorizontalLineFigure.java
@@ -0,0 +1,27 @@
+package org.protope.designer.web.figures;
+
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.draw2d.Figure;
+import org.eclipse.draw2d.Graphics;
+import org.eclipse.draw2d.geometry.Rectangle;
+
+public class WHorizontalLineFigure extends Figure {
+
+	public WHorizontalLineFigure() {
+		super();
+		setBackgroundColor(ColorConstants.tooltipBackground);
+		setForegroundColor(ColorConstants.tooltipForeground);
+	}
+
+	@Override
+	protected void paintFigure(Graphics graphics) {
+		Rectangle rect = getBounds().getCopy();
+
+		graphics.translate(getLocation());
+
+		graphics.drawLine(0, 0, rect.width, 0);
+
+		graphics.translate(getLocation().getNegated());
+	}
+
+}
