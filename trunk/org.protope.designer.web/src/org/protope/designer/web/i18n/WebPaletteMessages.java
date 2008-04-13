a3f83ed5fd87ac91626d979f0b8a79536178f537
hakandilek <hakandilek@2b12a20e-354a-0410-ac62-c160d17b8e0a>
2008-04-13 22:31:16 +0000

    Issue 10:Add web palette components

---
+++ trunk/org.protope.designer.web/src/org/protope/designer/web/i18n/WebPaletteMessages.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.protope.designer.web.i18n;
+
+import org.eclipse.osgi.util.NLS;
+
+public class WebPaletteMessages extends NLS {
+
+	public static String WebPalette_Tool_WHorizontalLine_Description;
+	public static String WebPalette_Tool_WHorizontalLine_Label;
+	public static String WebPalette_Tool_WHorizontalLine;
+	public static String WebPalette_Tool_WVerticalLine_Description;
+	public static String WebPalette_Tool_WVerticalLine_Label;
+	public static String WebPalette_Tool_WVerticalLine;
+	public static String WebPalette_Tool_WNote_Description;
+	public static String WebPalette_Tool_WNote_Label;
+	public static String WebPalette_Tool_WNote;
+	public static String WebPalette_Tool_WNote_Text;
+	public static String WebPalette_Tool_WImage_Description;
+	public static String WebPalette_Tool_WImage_Label;
+	public static String WebPalette_Tool_WImage;
+	public static String WebPalette_Tool_WTextEdit_Description;
+	public static String WebPalette_Tool_WTextEdit_Label;
+	public static String WebPalette_Tool_WTextEdit;
+	public static String WebPalette_Tool_WButton_Description;
+	public static String WebPalette_Tool_WButton_Label;
+	public static String WebPalette_Tool_WButton;
+	public static String WebPalette_Tool_WButton_Select_ActionLabel;
+	public static String WebPalette_Tool_WButton_Select_ActionTooltip;
+	public static String WebPalette_Tool_WButton_Unselect_ActionLabel;
+	public static String WebPalette_Tool_WButton_Unselect_ActionTooltip;
+	public static String WebPalette_Tool_WLabel_Description;
+	public static String WebPalette_Tool_WLabel_Label;
+	public static String WebPalette_Tool_WLabel;
+
+	public static String UIPlugin_Category_Components_Label;
+	public static String UIPlugin_Category_ControlGroup_Label;
+
+	static {
+		NLS
+				.initializeMessages(
+						"org.protope.designer.web.i18n.WebPaletteMessages", WebPaletteMessages.class); //$NON-NLS-1$
+	}
+
+}
