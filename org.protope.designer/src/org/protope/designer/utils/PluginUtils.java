747df933ec762759a0b9b181638f7029a2f42776
hakandilek <hakandilek@2b12a20e-354a-0410-ac62-c160d17b8e0a>
2008-04-19 21:49:04 +0000

    Issue 9: Finalize new project wizard

--- org.protope.designer/src/org/protope/designer/utils/PluginUtils.java
+++ org.protope.designer/src/org/protope/designer/utils/PluginUtils.java
@@ -2,6 +2,7 @@ package org.protope.designer.utils;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.MessageFormat;
 import java.util.Collections;
 
 import org.eclipse.core.resources.IWorkspace;
@@ -23,38 +24,37 @@ import org.protope.designer.plugin.ProtopeStatusConstants;
 
 public class PluginUtils {
 
-    /** the prefix of OSGi bundle locations */
-    public static final String BUNDLE_LOCATION_PREFIX = "initial@reference:";
+	/** the prefix of OSGi bundle locations */
+	public static final String BUNDLE_LOCATION_PREFIX = "initial@reference:";
 	public static final String IDE_WORKBENCH = "org.eclipse.ui.ide";
 
 	/**
-     * Checks if the bundle is ready for usage.
-     * 
-     * @param bundle
-     *            the bundle
-     * @return true if bundle is ready
-     */
-    public static boolean isReady(Bundle bundle)
-    {
-        return bundle != null && isReady(bundle.getState());
-    }
+	 * Checks if the bundle is ready for usage.
+	 * 
+	 * @param bundle
+	 *            the bundle
+	 * @return true if bundle is ready
+	 */
+	public static boolean isReady(Bundle bundle) {
+		return bundle != null && isReady(bundle.getState());
+	}
 
-    /**
-     * Checks if the bundle is ready for usage.
-     * 
-     * @param bundleState
-     *            the state of the bundle
-     * @see Bundle#RESOLVED
-     * @see Bundle#STARTING
-     * @see Bundle#ACTIVE
-     * @see Bundle#STOPPING
-     * 
-     * @return true if bundle is ready
-     */
-    public static boolean isReady(int bundleState)
-    {
-        return (bundleState & (Bundle.RESOLVED | Bundle.STARTING | Bundle.ACTIVE | Bundle.STOPPING)) != 0;
-    }
+	/**
+	 * Checks if the bundle is ready for usage.
+	 * 
+	 * @param bundleState
+	 *            the state of the bundle
+	 * @see Bundle#RESOLVED
+	 * @see Bundle#STARTING
+	 * @see Bundle#ACTIVE
+	 * @see Bundle#STOPPING
+	 * 
+	 * @return true if bundle is ready
+	 */
+	public static boolean isReady(int bundleState) {
+		return (bundleState & (Bundle.RESOLVED | Bundle.STARTING
+				| Bundle.ACTIVE | Bundle.STOPPING)) != 0;
+	}
 
 	public static URL getURL(String pluginID, String urlPath) {
 		if (pluginID == null) {
@@ -133,35 +133,39 @@ public class PluginUtils {
 		}
 
 		return url;
-		
+
 	}
 
 	public static Shell getActiveWorkbenchShell() {
-		 IWorkbenchWindow window= getActiveWorkbenchWindow();
-		 if (window != null) {
-		 	return window.getShell();
-		 }
-		 return null;
+		IWorkbenchWindow window = getActiveWorkbenchWindow();
+		if (window != null) {
+			return window.getShell();
+		}
+		return null;
 	}
-	
+
 	public static IWorkbenchWindow getActiveWorkbenchWindow() {
 		return getWorkbench().getActiveWorkbenchWindow();
 	}
-	
-    /**
-     * Returns the Platform UI workbench.  
-     * <p> 
-     * This method exists as a convenience for plugin implementors.  The
-     * workbench can also be accessed by invoking <code>PlatformUI.getWorkbench()</code>.
-     * </p>
-     * @return IWorkbench the workbench for this plug-in
-     */
-    public static IWorkbench getWorkbench() {
-        return PlatformUI.getWorkbench();
-    }
+
+	/**
+	 * Returns the Platform UI workbench.
+	 * <p>
+	 * This method exists as a convenience for plugin implementors. The
+	 * workbench can also be accessed by invoking
+	 * <code>PlatformUI.getWorkbench()</code>.
+	 * </p>
+	 * 
+	 * @return IWorkbench the workbench for this plug-in
+	 */
+	public static IWorkbench getWorkbench() {
+		return PlatformUI.getWorkbench();
+	}
 
 	public static void log(Throwable e) {
-		log(new Status(IStatus.ERROR, getPluginId(), ProtopeStatusConstants.INTERNAL_ERROR, ProtopeMessages.Plugin_internal_error, e)); 
+		log(new Status(IStatus.ERROR, getPluginId(),
+				ProtopeStatusConstants.INTERNAL_ERROR,
+				ProtopeMessages.Plugin_internal_error, e));
 	}
 
 	public static String getPluginId() {
@@ -172,6 +176,68 @@ public class PluginUtils {
 		getLog().log(status);
 	}
 
+	/**
+	 * Logs the given throwable to the platform log, indicating the class and
+	 * method from where it is being logged (this is not necessarily where it
+	 * occurred).
+	 * 
+	 * This convenience method is for internal use by the IDE Workbench only and
+	 * must not be called outside the IDE Workbench.
+	 * 
+	 * @param clazz
+	 *            The calling class.
+	 * @param methodName
+	 *            The calling method name.
+	 * @param t
+	 *            The throwable from where the problem actually occurred.
+	 */
+	public static void log(Class<?> clazz, String methodName, Throwable t) {
+		String msg = MessageFormat.format("Exception in {0}.{1}: {2}", //$NON-NLS-1$
+				new Object[] { clazz.getName(), methodName, t });
+		log(msg, t);
+	}
+
+	/**
+	 * Logs the given message and throwable to the platform log.
+	 * 
+	 * If you have a status object in hand call log(String, IStatus) instead.
+	 * 
+	 * This convenience method is for internal use by the IDE Workbench only and
+	 * must not be called outside the IDE Workbench.
+	 * 
+	 * @param message
+	 *            A high level UI message describing when the problem happened.
+	 * @param t
+	 *            The throwable from where the problem actually occurred.
+	 */
+	public static void log(String message, Throwable t) {
+		IStatus status = StatusUtils.newStatus(IStatus.ERROR, message, t);
+		log(message, status);
+	}
+
+	/**
+	 * Logs the given message and status to the platform log.
+	 * 
+	 * This convenience method is for internal use by the IDE Workbench only and
+	 * must not be called outside the IDE Workbench.
+	 * 
+	 * @param message
+	 *            A high level UI message describing when the problem happened.
+	 *            May be <code>null</code>.
+	 * @param status
+	 *            The status describing the problem. Must not be null.
+	 */
+	public static void log(String message, IStatus status) {
+
+		// 1FTUHE0: ITPCORE:ALL - API - Status & logging - loss of semantic info
+
+		if (message != null) {
+			log(StatusUtils.newStatus(IStatus.ERROR, message, null));
+		}
+
+		log(status);
+	}
+
 	protected static ILog getLog() {
 		return ProtopeDesignerPlugin.getDefault().getLog();
 	}
@@ -179,6 +245,5 @@ public class PluginUtils {
 	public static IWorkspace getWorkspace() {
 		return ResourcesPlugin.getWorkspace();
 	}
-	
 
 }
